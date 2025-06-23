9412c66e9f4a3603e2e2c5af8758be6a6f56a276
aziz <aziz@DESKTOP-BR0GG9C>
2025-06-23 21:04:59 +0300

    basic color structure and font

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,28 @@
 import React from 'react';
-import './App.css';
+import Header from "./sections/Header";
+import ProcessSteps from "./sections/ProcessSteps";
+import Services from "./sections/Services";
+import Mission from "./sections/Mission";
+import Team from "./sections/Team";
+import PartnershipForm from "./sections/PartnershipForm";
+import Footer from "./sections/Footer";
+import Benefits from "./sections/Benefits";
+import Values from "./sections/Values";
+import AboutBrand from "./sections/AboutBrand";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-
-      </header>
+        <Header/>
+        <AboutBrand/>
+        <ProcessSteps/>
+        <Benefits/>
+        <Services/>
+        <Mission/>
+        <Values/>
+        <Team/>
+        <PartnershipForm/>
+        <Footer/>
     </div>
   );
 }
---
+++ src/sections/ProcessSteps.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const ProcessSteps = () => {
+    return (
+        <div>
+            
+        </div>
+    );
+};
+
+export default ProcessSteps;

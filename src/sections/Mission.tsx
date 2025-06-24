ff4b5af93057c792e249f58ecff38c15cac264eb
aziz <aziz@DESKTOP-BR0GG9C>
2025-06-24 21:29:09 +0300

    part Mission

--- src/sections/Mission.tsx
+++ src/sections/Mission.tsx
@@ -2,8 +2,12 @@ import React from 'react';
 
 const Mission = () => {
     return (
-        <div>
-            
+        <div className="mission-container">
+            <section className="mission-section">
+                <p>Мы каждый день становимся ближе к нашей миссии</p>
+                <h2 className="mission-title">СОЗДАТЬ БУДУЩЕЕ ЗДОРОВОГО ПИТАНИЯ И СЕЛЬСКОГО ХОЗЯЙСТВА</h2>
+                <p>Ценности дают нам силу и направление, что делает нас сильнее и честнее</p>
+            </section>
         </div>
     );
 };
